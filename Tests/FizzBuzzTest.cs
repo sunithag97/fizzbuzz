e44bcd4d1defc800107f58deed883b63a8e05a8e
sunithag <sunithagadepally23@gmail.com>
2024-12-04 09:57:26 -0600

    added unit test

--- Tests/FizzBuzzTest.cs
+++ Tests/FizzBuzzTest.cs
@@ -1,28 +1,35 @@
-using FizzBuzzLibrary;
-using Xunit;
-
 namespace Tests;
+
+using Xunit;
+using FizzBuzzLibrary;
+using System.Collections.Generic;
+using System.Linq;
 
 public class FizzBuzzTest
 {
-    [Theory]
-    [InlineData(1)]
-    [InlineData(15)]
-    [InlineData(30)]
-    [InlineData(40)]
-    [InlineData(50)]
-    [InlineData(60)]
-    public void FizzBuzzTestResult(int val)
+    public static IEnumerable<object[]> GetFizzBuzz_ReturnsCorrectSequence()
     {
-        
-        var fizzBuzz = new FizzBuzz();
-        var result = fizzBuzz.GetFizzBuzz(val);
-
-        foreach (var r in result)
+        return new List<object[]>
         {
-            Console.WriteLine(r);
-        }
-        
-        
+            new object[]{new List<string>() { "1", "2", "fizz - 3", "4", "buzz - 5" }, 5},
+            new object[]{new List<string>() { "1", "2", "fizz - 3" }, 3},
+            new object[]{new List<string>() { "1" }, 1},
+            new object[]{new List<string>() { }, 0},
+            new object[]{ new List<string>(){"1", "2", "fizz - 3", "4", "buzz - 5", "fizz - 6", "7", "8", "fizz - 9", "buzz - 10", "11", "fizz - 12", "13", "14", "fizz & buzz - 15"}, 15 }
+        };
+    }
+   
+    [Theory]
+    [MemberData(nameof(GetFizzBuzz_ReturnsCorrectSequence))]
+    public void GetFizzBuzz_ReturnsCorrectSequence_ForUpperBound(List<string> expected, int upperBoundVal)
+    {
+        // Arrange
+        var fizzBuzz = new FizzBuzz();
+       
+        // Act
+        var result = fizzBuzz.GetFizzBuzz(upperBoundVal).ToList();
+
+        // Assert
+        Assert.Equal(expected, result);
     }
 }
